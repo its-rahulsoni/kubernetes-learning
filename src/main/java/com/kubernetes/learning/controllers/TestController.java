432d32906d4f2faed2d78bae53fce512e8ed84e0
Rahul Soni <rahulsoni.myventures@gmail.com>
2025-06-21 13:47:11 +0530

    kubernetes deployment Manifests files

--- src/main/java/com/kubernetes/learning/controllers/TestController.java
+++ src/main/java/com/kubernetes/learning/controllers/TestController.java
@@ -21,29 +21,29 @@ public class TestController {
     @GetMapping("/hello")
     public String sayHello() {
         logger.info("Api /hello is invoked to say - Hello, World!.");
-        return "Service gha-deploy2 says - Hello, World!";
+        return "Service kubernetes-learning says - Hello, World!";
     }
 
     // A simple GET request to respond with a healthy instance message ....
     @GetMapping("/health")
     public String sayHealthy() {
         logger.info("Api /health is invoked to say - I am Healthy!.");
-        return "Service gha-deploy2 says - I am Healthy!";
+        return "Service kubernetes-learning says - I am Healthy!";
     }
 
 
     // A simple GET request with a greetings message ....
     @GetMapping("/greetings")
     public String greetings() {
-        logger.info("Api /greetings is invoked to say - Greetings from Service gha-deploy2. Hope you have a wonderful day ahead..");
-        return "Api /greetings is invoked to say - Greetings from Service gha-deploy2. Hope you have a wonderful day ahead.";
+        logger.info("Api /greetings is invoked to say - Greetings from Service kubernetes-learning. Hope you have a wonderful day ahead..");
+        return "Api /greetings is invoked to say - Greetings from Service kubernetes-learning. Hope you have a wonderful day ahead.";
     }
 
     // A simple GET request to say hello to the world ....
     @GetMapping("/bye")
     public String sayBye() {
         logger.info("Api /bye is invoked to say - Bye, World!.");
-        return "Service gha-deploy2 says - Bye, World!";
+        return "Service kubernetes-learning says - Bye, World!";
     }
 
 }
